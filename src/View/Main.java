abacf8dde3fdfb7de7bf04b71e8d97b095dc9d72
JordanJohns <jdj1@sfu.ca>
2024-02-22 21:02:20 -0800

    Setup Main.java

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        
+        if (args.length == 0) args = new String[] {"5"};    // Default to 5 enemies if not given any args
+        UserInterface userInterface = new UserInterface();
+        userInterface.startGame(Integer.parseInt(args[0]));
     }
 }
