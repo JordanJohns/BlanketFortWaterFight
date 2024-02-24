06fdf70b0abcd619cbd8cec6982ce7f332b36336
TheGreatMarksman <151799337+TheGreatMarksman@users.noreply.github.com>
2024-02-23 21:50:23 -0800

    Update Polyomino.java
    Now mostly works, will occasionally throw an exception when making 10 forts

--- src/Model/Polyomino.java
+++ src/Model/Polyomino.java
@@ -1,48 +1,105 @@
 package Model;
 
-import java.util.random.*;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.concurrent.ThreadLocalRandom;
 
+import javax.management.RuntimeErrorException;
+
 public class Polyomino {
-    final int SIZE = 5;
-    private Coordinate[] positions;
-    public Polyomino(char[][] map){
-        Coordinate startingPosition = findStartingPosition(map);
-        for(int i = 0; i < SIZE; i++){
-            
+    private final int SIZE = 5;
+    private List<Coordinate> positions = new ArrayList<>();
+    private char[][] map;
+    private char letter;
+
+    public Polyomino(char[][] map, char letter) {
+        this.map = map;
+        this.letter = letter;
+        generate();
+    }
+
+    private void generate() {
+        Coordinate startingPosition = findStartingPosition();
+        positions.add(new Coordinate(startingPosition));
+        int x = startingPosition.getX();
+        int y = startingPosition.getY();
+        map[x][y] = letter;
+        for (int i = 1; i < SIZE; i++) {
+            Coordinate newPosition = findNewPosition();
+            positions.add(newPosition);
+            x = newPosition.getX();
+            y = newPosition.getY();
+            map[x][y] = letter;
         }
     }
-    /*
-    public void generate(char[] map, char letter){
-        
-    }
-    */
-    public Coordinate findStartingPosition(char[][] map){
+
+    private Coordinate findStartingPosition() {
         int x;
         int y;
         Coordinate position;
         int testBreakNum = 0;
-        while(true){
+        while (true) {
             x = ThreadLocalRandom.current().nextInt(0, map.length - 1);
             y = ThreadLocalRandom.current().nextInt(0, map.length - 1);
-            if(map[x][y] != ' '){
+            if (map[x][y] == '.') {
                 position = new Coordinate(x, y);
                 break;
             }
             testBreakNum++;
-            if(testBreakNum >= 100){
-                System.out.println("Infinite loop");
-                position = new Coordinate(0, 0);
-                break;
+            // Test statement
+            if (testBreakNum >= 500) {
+                throw new RuntimeException("ERROR: Map is full");
             }
         }
         return position;
     }
 
-    public boolean Contains(Coordinate position) {
-        for (int i = 0; i < positions.length; i++) {
-            if (position.equals(positions[i])) {
+    private Coordinate findNewPosition() {
+        Coordinate selectedPosition;
+        int testBreakNum = 0;
+        while (true) {
+            if (positions.size() > 1) {
+                int randNum = ThreadLocalRandom.current().nextInt(0, positions.size() - 1);
+                selectedPosition = positions.get(randNum);
+            } else {
+                selectedPosition = positions.get(0);
+            }
+            int x = selectedPosition.getX();
+            int y = selectedPosition.getY();
+            if (validPosition(x - 1, y)) {
+                selectedPosition = new Coordinate(x - 1, y);
+                break;
+            } else if (validPosition(x, y - 1)) {
+                selectedPosition = new Coordinate(x, y - 1);
+                break;
+            } else if (validPosition(x + 1, y)) {
+                selectedPosition = new Coordinate(x + 1, y);
+                break;
+            } else if (validPosition(x, y + 1)) {
+                selectedPosition = new Coordinate(x, y + 1);
+                break;
+            }
+            if (testBreakNum >= 100) {
+                throw new RuntimeException("ERROR: Insufficent space for Polyomino");
+            }
+            testBreakNum++;
+        }
+        return selectedPosition;
+    }
+
+    private boolean validPosition(int x, int y) {
+        if (x < 0 || x >= 10 || y < 0 || y >= 10) {
+            return false;
+        }
+        if (map[x][y] != '.') {
+            return false;
+        }
+        return true;
+    }
+
+    public boolean contains(Coordinate position) {
+        for (int i = 0; i < positions.size(); i++) {
+            if (position.equals(positions.get(i))) {
                 return true;
             }
         }
